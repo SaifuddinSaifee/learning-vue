ce32bbe012b25aed20224d00dfcec8b19cfe8b85
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2023-12-03 22:34:20 +0530

    Form Complete

--- vue-forms/src/components/SignUpForm.vue
+++ vue-forms/src/components/SignUpForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form>
+    <form class="form" @submit.prevent="handleSubmit">
         <div class="info email">
             <label for="email">Email</label>
             <input type="email" required v-model="email" />
@@ -16,7 +16,44 @@
                 <option value="UX Researcher">UX Researcher</option>
             </select>
         </div>
-        <p>role is : {{ role }}</p>
+        <div class="terms">
+            <input v-model="terms" type="checkbox" name="term" id="term">
+            <label for="term">Accept terms and conditions</label>
+        </div>
+        <div class="true" v-if=terms>
+            <p class="message">Congratulations! You may now proceed🚀</p>
+        </div>
+        <div class="names">
+            <div class="name">
+                <input type="checkbox" name="" value="Choice 1" v-model="names">
+                <label for="Choice 1">Choice 1</label>
+            </div>
+            <div class="name">
+                <input type="checkbox" name="" value="Choice 2" v-model="names">
+                <label for="Choice 1">Choice 2</label>
+            </div>
+            <div class="name">
+                <input type="checkbox" name="" value="Choice 3" v-model="names">
+                <label for="Choice 1">Choice 3</label>
+            </div>
+            <div class="name">
+                <input type="checkbox" name="" value="Choice 4" v-model="names">
+                <label for="Choice 1">Choice 4</label>
+            </div>
+            <div class="name">
+                <input type="checkbox" name="" value="Choice 5" v-model="names">
+                <label for="Choice 1">Choice 5</label>
+            </div>
+        </div>
+        <div class="info">
+            <label for="Skill">Skill</label>
+            <input class="Skill" type="Skill" v-model="tempSkill" @keyup.alt="addSkill" />
+            <div class="skillList">
+                <p class="skills pill" v-for="skill in skills" :key="skill" @click="removeSkill(skill)">{{ skill }}</p>
+            </div>
+            <div v-if="exists">Skill already exists</div>
+        </div>
+        <button type="submit" class="submit-button">Let's Go🚀</button>
     </form>
 </template>
 
@@ -27,46 +64,116 @@ export default {
         return {
             email: '',
             password: '',
-            role: ''
+            role: 'Project manager',
+            terms: true,
+            names: [],
+            tempSkill: '',
+            skills: [],
+            exists: false,
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if (e.key === "Enter" && this.tempSkill && !this.skills.includes(this.tempSkill)) {
+                this.exists = false;
+                this.skills.push(this.tempSkill)
+                this.tempSkill = ''
+            } else if (this.skills.includes(this.tempSkill)) {
+                this.exists = true;
+                this.tempSkill = ''
+            }
+        },
+        removeSkill(skill) {
+            const index = this.skills.indexOf(skill);
+            if (index > -1) {
+                this.skills.splice(index, 1);
+            }
+        },
+        handleSubmit () {
+            console.log("Form submitted");
         }
     }
 };
 
 </script>
 
+<!-- ############################################################################################## -->
 <style scoped>
 form {
+    color: rgb(255, 255, 255);
     font-family: 'Roboto', sans-serif;
     width: 340px;
     margin: 0 auto;
-    background-color: #fff;
-    padding: 2rem;
-    border-radius: 8px;
+    background: linear-gradient(60deg, #5f0f4077, #310e687e);
+    padding: 3rem;
+    border-radius: 16px;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border: 1px solid rgba(245, 245, 245, 0.43);
+
 }
 
 .info,
 .role {
-    margin: 1rem;
+    margin: 1rem 0;
 }
 
 /* Form input fields */
 input[type="text"],
 input[type="email"],
 input[type="password"],
-textarea {
+textarea,
+select,
+.Skill {
     width: 100%;
     padding: 12px 20px;
     margin: 8px 0;
     display: inline-block;
-    border: 1px solid #ccc;
+    border: none;
+    border-bottom: 1px solid #ccc;
     box-sizing: border-box;
     font-size: 16px;
+    background-color: transparent;
+    color: white;
+    border-radius: 21px;
+}
+
+.true {
+    /* width: 100%; */
+    height: 1.5rem;
+    padding: 0.5rem;
+    background: rgba(0, 128, 0, 0.208);
+    color: green;
     border-radius: 4px;
+    margin-top: 1rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 14px;
+}
+
+.pill {
+    background-color: purple;
+    color: white;
+    height: 26px;
+    padding: 0 16px;
+    border-radius: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 6px;
+}
+
+.terms,
+.name,
+.skillList {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: wrap;
 }
 
 /* Form labels */
@@ -78,19 +185,20 @@ label {
 
 /* Form submit button */
 button[type="submit"] {
-    background-color: #4285f4;
+    width: 100%;
+    height: 2.5rem;
+    border-radius: 1.25rem;
+    background-color: #5f0f40;
+    border: 1px solid white;
+    transition: 300ms;
     color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
     font-size: 16px;
-    border-radius: 4px;
+    font-weight: 500;
 }
 
 /* Form submit button hover effect */
 button[type="submit"]:hover {
-    background-color: #3367d6;
+    background-color: #310e68;
 }
 
 /* Form error message */
